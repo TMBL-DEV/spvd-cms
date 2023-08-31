7a99911586d0790e934a7b19d896555e1cf25644
TMBL-DEV <timblommesteijn@gmail.com>
2023-08-31 16:07:40 +0200

    WIP - disabled home api route - wrote out new composable for fetching from strapi

--- nuxt/server/api/pages/home.ts
+++ nuxt/server/api/pages/home.ts
@@ -3,6 +3,6 @@ import * as blogType from '../../../types/blog';
 import { Strapi4ResponseMany } from '@nuxtjs/strapi/dist/runtime/types';
 
 export default defineEventHandler(async (event) => {
-    const response = await server.useStrapiFetch<Strapi4ResponseMany<blogType.blog>>('blogs');
-    return response;
+    // const response = await server.useStrapiFetch<Strapi4ResponseMany<blogType.blog>>('blogs');
+    // return response;
 });
--- nuxt/server/utils/serverStrapi.ts
+++ nuxt/server/utils/serverStrapi.ts
@@ -1,26 +1,59 @@
 type strapiFetchOptions = {
-    params: Record<string, any>;
+    // params: Record<string, any>;
     token: string;
     baseUrl: string;
 };
 
-export const useStrapiFetch = async <T = unknown>(
-    entity: string,
-    options: strapiFetchOptions = {
-        params: {},
-        token: useRuntimeConfig().public.strapiToken,
-        baseUrl: useRuntimeConfig().public.strapiBaseUrl,
-    }
-) => {
-    const url = `http://strapi/api/${entity}`;
-    const params = new URLSearchParams();
-    const response = await fetch(url + params, {
-        headers: {
-            Authorization: `Bearer ${options.token}`,
-        },
-    });    
+type requestData = {
+    params?: Record<string, any>;
+    body?: Record<string, any>;
+}
+// export const useStrapiFetch = async <T = unknown>(
+//     entity: string,
+//     options: strapiFetchOptions = {
+//         params: {},
+//         token: useRuntimeConfig().public.strapiToken,
+//         baseUrl: useRuntimeConfig().public.strapiBaseUrl,
+//     }
+// ) => {
+//     const url = `http://strapi/api/${entity}`;
+//     const params = new URLSearchParams();
+//     const response = await fetch(url + params, {
+//         headers: {
+//             Authorization: `Bearer ${options.token}`,
+//         },
+//     });    
 
-    const data: T|null = await response.json()
+//     const data: T|null = await response.json()
     
-    return data; 
-};
+//     return data; 
+// };
+
+
+const useStrapiFetch = (options: strapiFetchOptions) => {
+   return {
+        get: (url: string, data: requestData) => useFetch(`${options.baseUrl}${url}`, {     
+            method: 'get',
+            params: data.params,
+        }),
+        post: (url: string, data: requestData) => useFetch(`${options.baseUrl}${url}`, {
+            method: 'post',
+            params: data.params,
+            body: data.body
+        }),
+        patch: (url: string, data: requestData) => useFetch(`${options.baseUrl}${url}`, {
+            method: 'patch',
+            params: data.params,
+            body: data.body
+        }),
+        put: (url: string, data: requestData) => useFetch(`${options.baseUrl}${url}`, {
+            method: 'put',
+            params: data.params,
+            body: data.body
+        }),
+        delete: (url: string, data: requestData) => useFetch(`${options.baseUrl}${url}`, {
+            method: 'delete',
+            params: data.params,
+        }),
+   }
+}
